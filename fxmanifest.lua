5731eea7b520c38972e63aa46e756270031528af
Joe? <72747023+dbcaa@users.noreply.github.com>
2025-11-28 14:18:31 -0600

    Bump version from 1.0.0 to 1.1.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 author 'dbca.'
 description 'A complete CarPlay-inspired music system for FiveM with xsound integration'
-version '1.0.0'
+version '1.1.0'
 
 shared_scripts {
     'shared/config.lua'
